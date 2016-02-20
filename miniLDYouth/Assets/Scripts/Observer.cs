71e97ad6969558495e6764d0fa33cdbb7b89efb5
Andy Perdana <geruhn@hotmail.com>
2016-02-20 19:49:46 +0100

    Added GameController, advanced Spawner and Geraffel

---
+++ miniLDYouth/Assets/Scripts/Observer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    public interface Observer
+    {
+        void getInfo(IInfo info);
+    }
+}
