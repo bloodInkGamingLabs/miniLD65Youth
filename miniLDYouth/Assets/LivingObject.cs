661983a22949464a0ea9553092f7e8dce5370274
dojo1017 <dojo1017@hs-karlsruhe.de>
2016-02-20 12:22:03 +0100

    added BabyStage2D

---
+++ miniLDYouth/Assets/LivingObject.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public abstract class LivingObjectClass : MonoBehaviour
+{
+    #region healthAttributes
+    public float healthMax_init = 100.0f;
+
+    private float _healthMax = 100.0f;
+    private float _health;
+    public float health { get { return _health; } }
+    public float healthMax { get { return _healthMax; } }
+    #endregion
+    #region healthMethods
+    /// <summary>
+    /// Setzt das Leben auf den höchsten Wert.
+    /// </summary>
+    public void healthRefill()
+    {
+        this._health = this._healthMax;
+    }
+    /// <summary>
+    /// Erhöht das Leben um den angegebenen Wert.
+    /// </summary>
+    /// <param name="by"></param>
+    public void healthRefill(float by)
+    {
+        this._health = normalizeHealth(this._health + by);
+    }
+    /// <summary>
+    /// Verringert das Leben um den angegebenen Wert.
+    /// </summary>
+    /// <param name="by"></param>
+    public void healthDecrease(float by)
+    {
+        this._health = normalizeHealth(this._health - by);
+    }
+
+    private float normalizeHealth(float newValue)
+    {
+        //Berechnet einen erlaubten Wert im Bereich 0 bis healthMax
+        return newValue > _healthMax ? _healthMax : (newValue < 0.0f ? 0.0f : newValue);
+    }
+    #endregion
+
+    #region stats
+
+    #endregion
+
+
+    public Boolean isAlive() { return health > 0f; }
+    public LivingObjectClass()
+    {
+        _health = _healthMax = healthMax_init;
+    }
+}
---
+++ miniLDYouth/Assets/player.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class player : LivingObjectClass {
+    #region exp
+    public long exp_init = 0;
+    private long _exp;
+    private float _expModifierPerLevel = 1.5f;
+    #endregion
+
+    #region level
+    public int level_init = 1;
+    private int _level;
+    public int level { get { return _level; } }
+    public void levelUp()
+    {
+        if (canLevelUp())
+        {
+            _level++;
+            //add some magical effects
+        }
+    }
+    private bool canLevelUp()
+    {
+        return _exp > Math.Pow(_expModifierPerLevel, level);
+    }
+    #endregion
+
+    #region stats
+    private float _movementSpeed = 5.0f;
+    public float movementSpeed { get { return _movementSpeed; } }
+
+    #endregion
+
+
+    public player() : base()
+    {
+        _level = level_init;
+        _exp = exp_init;
+    }
+  
+
+    public void move()
+    {
+        Boolean left = Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.LeftArrow);
+        Boolean right = Input.GetKey(KeyCode.D) | Input.GetKey(KeyCode.RightArrow);
+        Boolean up = Input.GetKey(KeyCode.W) | Input.GetKey(KeyCode.UpArrow);
+        Boolean down = Input.GetKey(KeyCode.S) | Input.GetKey(KeyCode.DownArrow);
+        float updownSpeedModifier = 0.625f; //Simuliert quadratisches Spielfeld bei 16:9 Format
+        float diagonalSpeedModifier = 0.727f;//nur bei 45° Bewegungen gültig!
+        Boolean diagonal = (up | down) & (left | right);
+
+
+        if (left & !right)
+        {
+            transform.position += Vector3.left * Time.deltaTime * this.movementSpeed * (diagonal ? diagonalSpeedModifier : 1.0f);
+        }
+        else if (!left & right)
+        {
+            transform.position += Vector3.right * Time.deltaTime * this.movementSpeed * (diagonal ? diagonalSpeedModifier : 1.0f);
+        }
+        
+
+        if(up & !down)
+        {
+            transform.position += Vector3.up * Time.deltaTime * this.movementSpeed * updownSpeedModifier * (diagonal ? diagonalSpeedModifier : 1.0f);
+        } else if(!up & down)
+        {
+            transform.position += Vector3.down * Time.deltaTime * this.movementSpeed * updownSpeedModifier * (diagonal ? diagonalSpeedModifier : 1.0f);
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        move();
+        levelUp();//triggern von expgain
+	}
+}
